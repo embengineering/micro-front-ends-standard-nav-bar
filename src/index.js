e7c1b78d4265efa3ab09af03b2522128fd66e62f
Emmanuel Morales <emmanuel.morales@headspring.com>
2018-04-10 15:36:24 -0500

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
-// styles
 import './index.css';
-
-// web components
 import './navbar';
-
-// other stuff
-import registerServiceWorker from './registerServiceWorker';
-
-registerServiceWorker();
